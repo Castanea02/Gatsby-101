5c31e0db6ff010ede958d7a083f5c996cbec680a
castanea02 <ahrhk2000@gmail.com>
2024-02-18 06:02:20 +0900

    Gatsby Image

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import Seo from "../../components/Seo";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 interface IBlogPostProps {
   data: Queries.PostDetailQuery;
@@ -9,9 +10,13 @@ interface IBlogPostProps {
 }
 
 export default function BlogPost({ data, children }: IBlogPostProps) {
-  console.log(children);
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
+
   return (
     <Layout title="Blog Post">
+      <GatsbyImage image={image as any} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -20,18 +25,22 @@ export default function BlogPost({ data, children }: IBlogPostProps) {
 export const query = graphql`
   query PostDetail($frontmatter__slug: String) {
     mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
-      body
       frontmatter {
         author
         category
         date
         slug
         title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 300, placeholder: TRACED_SVG)
+          }
+        }
       }
     }
   }
 `;
 
 export const Head = ({ data }: IBlogPostProps) => (
-  <Seo title={data.mdx?.frontmatter?.title} />
+  <Seo title={data.mdx?.frontmatter?.title as any} />
 );
